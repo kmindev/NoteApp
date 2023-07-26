7c9d1de19287b25caf575e0ca6f66af19b1cbfe3
kmindev <kangkm0324@gmailcom>
2023-07-26 14:37:40 +0900

    Feat: "회원가입, 로그인 API 구현"

--- back/src/main/java/com/example/back/controller/UserController.java
+++ back/src/main/java/com/example/back/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.example.back.controller;
 
+import com.example.back.domain.User;
 import com.example.back.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
     private final UserService userService;
 
@@ -12,4 +16,16 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
+    @PostMapping("/join")
+    public String join(User user) {
+        return userService.join(user);
+    }
+
+    @PostMapping("/login")
+    public String login(User user) {
+        return userService.login(user);
+    }
+
+
 }
--- back/src/main/java/com/example/back/repository/UserRepository.java
+++ back/src/main/java/com/example/back/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.example.back.repository;
 import com.example.back.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByUserIdAndUserPw(String userId, String userPw);
+
 }
